fb3e96c1d6cb0d20520ed8719385aa207455508c
Dorota Pawłowska <dpawlowska@op.pl>
2018-08-19 19:25:46 +0200

    completed thirteenth project: Slide In on Scroll

---
+++ JavaScript30/SlideInOnScroll_13/index.js
@@ -0,0 +1,48 @@
+
+var wait = 20;
+var immediate = true;
+
+//function debounce(func, wait = 20, immediate = true){
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+    // console.log(e);
+    // console.log(window.scrollY);
+    // console.count(e);
+    sliderImages.forEach(function (sliderImage) {
+        //half way through the image
+        const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+        //bottom of the image
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+        // console.log(slideInAt);
+        // console.log(imageBottom);
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+
+        if(isHalfShown && isNotScrolledPast){
+            sliderImage.classList.add('active');
+        }else{
+            sliderImage.classList.remove('active');
+        }
+
+
+    });
+
+}
+
+window.addEventListener('scroll', debounce(checkSlide, wait, immediate));
