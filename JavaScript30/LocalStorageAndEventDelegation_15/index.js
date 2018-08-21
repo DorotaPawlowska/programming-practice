04f4a59eeb626d309caef5e1a1770a88c8cdab34
Dorota Pawłowska <dpawlowska@op.pl>
2018-08-21 19:13:39 +0200

    initial commit for fifteenth project: LocalStorage and Event Delegation

---
+++ JavaScript30/LocalStorageAndEventDelegation_15/index.js
@@ -0,0 +1,11 @@
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+const items = [];
+
+function addItem(e) {
+    e.preventDefault();
+    console.log('helo')
+}
+
+
+addItems.addEventListener('submit', addItem);
