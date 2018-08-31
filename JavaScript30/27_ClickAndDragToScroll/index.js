4148d3c99bde14408fb20e9be8c067ca1a5d3a71
Dorota Pawłowska <dpawlowska@op.pl>
2018-08-31 23:38:16 +0200

    completed twenty-seventh project: Click and Drag to Scroll

---
+++ JavaScript30/27_ClickAndDragToScroll/index.js
@@ -0,0 +1,40 @@
+const slider = document.querySelector('.items');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+    // console.log(e.pageX);
+    // console.log(startX);
+});
+
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+    // console.log(e.pageX);
+});
+
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+    // console.log(e.pageX);
+});
+
+slider.addEventListener('mousemove', (e) => {
+    if(!isDown) return; // stop the fn from running
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    // console.log({x, startX});
+    const walk = (x - startX) * 3;
+    // console.log(walk);
+    slider.scrollLeft = scrollLeft - walk;
+
+    // console.count(isDown);
+    // console.log(startX);
+});
+
+
