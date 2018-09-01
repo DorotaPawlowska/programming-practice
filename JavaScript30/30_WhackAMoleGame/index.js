bcdde9bee7acde253d7308db143c14cb340e41cd
Dorota Pawłowska <dpawlowska@op.pl>
2018-09-01 19:31:44 +0200

    completed thirtieth project: Whack A Mole Game
    more fun to do with that game in the future !!!!

---
+++ JavaScript30/30_WhackAMoleGame/index.js
@@ -0,0 +1,56 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+let lastHole;
+let timeUp = false;
+let score = 0;
+
+function randomTime(min, max) {
+    return Math.round(Math.random() * (max -min) + min);
+}
+
+function randomHole(holes) {
+    // console.log(holes.length);
+    const idx = Math.floor(Math.random() * holes.length);
+    const hole = holes[idx];
+
+    if(hole === lastHole){
+        console.log('ta sama dziura!');
+        return randomHole(holes);
+    }
+    // console.log(hole);
+
+    lastHole = hole;
+    return hole;
+}
+
+function peep() {
+    const time = randomTime(200, 1000);
+    const hole = randomHole(holes);
+
+    hole.classList.add('up');
+    // console.log(time, hole);
+
+    setTimeout(() => {
+        hole.classList.remove('up');
+        if(!timeUp) peep();
+    }, time);
+}
+
+function startGame() {
+    scoreBoard.textContent = 0;
+    timeUp = false;
+    score = 0;
+    peep();
+    setTimeout(() => timeUp = true, 10000);
+}
+
+function bonk(e) {
+    if(!e.isTrusted) return; // cheater !!!
+    // console.log(e);
+    score++;
+    this.classList.remove('up');
+    scoreBoard.textContent = score;
+}
+
+moles.forEach( mole => mole.addEventListener('click', bonk));
