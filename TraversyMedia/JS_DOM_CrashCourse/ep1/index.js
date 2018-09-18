270ccd6cedb67a101f229763d7d6e398a1c92431
Dorota Pawłowska <dpawlowska@op.pl>
2018-09-18 23:21:08 +0200

    JavaScript DOM Crash Course - Part 1 first part of this episode -> '17:19'
    Changes to be committed:
    	new file:   TraversyMedia/JS_DOM_CrashCourse/ep1/index.html
    	new file:   TraversyMedia/JS_DOM_CrashCourse/ep1/index.js

---
+++ TraversyMedia/JS_DOM_CrashCourse/ep1/index.js
@@ -0,0 +1,25 @@
+
+// console.dir(document);
+
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// // document.title = 123;
+// // console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// // document.all[10].textContent = 'Hello';
+// console.log(document.forms[0]);
+// console.log(document.links);
+
+// console.log(document.images);
+
+// selektory =========================================================================
+
+console.log(document.getElementById('header-title'));
+var headerTitile = document.getElementById('header-title');
+console.log(headerTitile);
+headerTitile.textContent = 'Hello';
