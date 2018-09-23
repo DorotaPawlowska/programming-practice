5ebdd2861cd76ae18066c4fa306e8c96c688daa7
Dorota Pawłowska <dpawlowska@op.pl>
2018-09-23 15:50:54 +0200

    JavaScript DOM Crash Course - Part 3 last part
    Changes to be committed:
    	renamed:    TraversyMedia/JS_DOM_CrashCourse/ep1+ep2/index.html -> TraversyMedia/JS_DOM_CrashCourse/ep1+ep2+ep3/index.html
    	renamed:    TraversyMedia/JS_DOM_CrashCourse/ep1+ep2/index.js -> TraversyMedia/JS_DOM_CrashCourse/ep1+ep2+ep3/index.js

--- TraversyMedia/JS_DOM_CrashCourse/ep1+ep2+ep3/index.js
+++ TraversyMedia/JS_DOM_CrashCourse/ep1+ep2+ep3/index.js
@@ -171,11 +171,86 @@
 
 // =======================traversing the DOM - eventListners========================================================
 
-var button = document.getElementById('button').addEventListener('click', buttonClick);
+// var button = document.getElementById('button').addEventListener('click', buttonClick);
 
-function buttonClick(){
+// function buttonClick(e){
   // console.log('button clicked');
-  document.getElementById('header-title').textContent = 'Changed';
-  document.querySelector('#main').style.backgroundColor = '#f4f4f4';
-}
+  // document.getElementById('header-title').textContent = 'Changed';
+  // document.querySelector('#main').style.backgroundColor = '#f4f4f4';
+  // console.log(e);
 
+  // console.log(e.target);
+  // console.log(e.target.id);
+  // console.log(e.target.className);
+  // console.log(e.target.classList);
+  // var output = document.getElementById('output');
+  // output.innerHTML = '<h3>' + e.target.id + '</h3>';
+
+  // console.log(e.type);
+  // console.log(e.clientX);
+  // console.log(e.clientY);
+
+    // console.log(e.offsetX);
+    // console.log(e.offsetY);
+
+    // console.log(e.altKey);
+    // console.log(e.ctrlKey);
+    // console.log(e.shiftKey);
+
+// }
+
+var button = document.getElementById('button');
+var box = document.getElementById('box');
+
+  // var output = document.getElementById('output');
+  // output.innerHTML = '<h3>' + e.target.id + '</h3>';
+
+// button.addEventListener('click', runEvent);
+// button.addEventListener('dblclick', runEvent);
+// button.addEventListener('mousedown', runEvent);
+// button.addEventListener('mouseup', runEvent);
+
+// box.addEventListener('mouseenter', runEvent);
+// box.addEventListener('mouseleave', runEvent);
+
+// box.addEventListener('mouseover', runEvent);
+// box.addEventListener('mouseout', runEvent);
+
+// box.addEventListener('mousemove', runEvent);
+
+var itemInput = document.querySelector('input[type="text"]');
+var form = document.querySelector('form');
+var select = document.querySelector('select');
+
+// itemInput.addEventListener('keydown', runEvent);
+// itemInput.addEventListener('keyup', runEvent);
+// itemInput.addEventListener('keypress', runEvent);
+
+// itemInput.addEventListener('focus', runEvent);
+// itemInput.addEventListener('blur', runEvent);
+
+// itemInput.addEventListener('cut', runEvent);
+// itemInput.addEventListener('paste', runEvent);
+
+// itemInput.addEventListener('input', runEvent);
+
+// select.addEventListener('change', runEvent);
+// select.addEventListener('input', runEvent);
+
+form.addEventListener('submit', runEvent);
+
+function runEvent(e){
+  e.preventDefault();
+  console.log('event type: '+ e.type);
+
+  // console.log(e.target.value);
+
+  // document.body.style.display = 'none'; // wszystko znika !!!!
+
+  // console.log(e.target.value);
+  // document.getElementById('output').innerHTML = '<h3>' + e.target.value + '</h3>';
+
+  // output.innerHTML = '<h3>MouseX:' +e.offsetX + '</h3><h3>MouseY: '+ e.offsetY + '</h3>';
+
+  // document.body.style.backgroundColor = 'rgb('+e.offsetX+','+e.offsetY+',40)';
+}
