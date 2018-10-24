075be13542fa59cafa9f8a18dda150828455fc29
Dorota Pawłowska <dpawlowska@op.pl>
2018-10-24 20:29:39 +0200

    Full Screen Image Slider With HTML, CSS & JS
    Changes to be committed:
    	new file:   TraversyMedia/FullScreenImageSliderWithHTML_CSS&JS/img/img1.jpeg
    	new file:   TraversyMedia/FullScreenImageSliderWithHTML_CSS&JS/img/img2.jpeg
    	new file:   TraversyMedia/FullScreenImageSliderWithHTML_CSS&JS/img/img3.jpeg
    	new file:   TraversyMedia/FullScreenImageSliderWithHTML_CSS&JS/index.html
    	new file:   TraversyMedia/FullScreenImageSliderWithHTML_CSS&JS/main.css
    	new file:   TraversyMedia/FullScreenImageSliderWithHTML_CSS&JS/main.js

---
+++ TraversyMedia/FullScreenImageSliderWithHTML_CSS&JS/main.js
@@ -0,0 +1,50 @@
+let sliderImages = document.querySelectorAll('.slide'),
+    arrowRight = document.querySelector('#arrow-right'),
+    arrowLeft = document.querySelector('#arrow-left'),
+    current = 0;
+
+//clear all images
+    function reset(){
+      for(let i = 0; i < sliderImages.length; i++){
+        sliderImages[i].style.display = 'none';
+      }
+    }
+//init slider
+    function startSlide(){
+      reset();
+      sliderImages[0].style.display = 'block';
+    }
+
+//show prev
+    function slideLeft(){
+      reset();
+      sliderImages[current - 1].style.display = 'block';
+      current--;
+    }
+
+//left arrow click
+    arrowLeft.addEventListener('click', () => {
+      if(current === 0){
+        current = sliderImages.length;
+      }
+      slideLeft();
+    });
+
+//show next
+    function slideRight(){
+      reset();
+      sliderImages[current + 1].style.display = 'block';
+      current++;
+    }
+
+//right arrow click
+arrowRight.addEventListener('click', () => {
+  if(current === sliderImages.length - 1){
+    current = -1;
+  }
+  slideRight();
+});
+
+
+
+    startSlide();
