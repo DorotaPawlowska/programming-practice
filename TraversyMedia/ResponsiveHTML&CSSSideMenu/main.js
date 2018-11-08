755223ef8768569ff161dcb182f0b6a6f4d7f1be
Dorota Pawłowska <dpawlowska@op.pl>
2018-11-08 20:37:03 +0100

    Responsive HTML & CSS Side Menu From Scratch
    Changes to be committed:
    	new file:   TraversyMedia/ResponsiveHTML&CSSSideMenu/index.html
    	new file:   TraversyMedia/ResponsiveHTML&CSSSideMenu/main.css
    	new file:   TraversyMedia/ResponsiveHTML&CSSSideMenu/main.js

---
+++ TraversyMedia/ResponsiveHTML&CSSSideMenu/main.js
@@ -0,0 +1,10 @@
+
+function openSlideMenu(){
+  document.getElementById('side-menu').style.width = '250px';
+  document.getElementById('main').style.marginLeft = '250px';
+}
+
+function closeSlideMenu(){
+  document.getElementById('side-menu').style.width = '0px';
+  document.getElementById('main').style.marginLeft = '0px';
+}
