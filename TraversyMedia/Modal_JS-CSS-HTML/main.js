52c467dedfeb3a9f7adfc90163dc50c054337967
Dorota Pawłowska <dpawlowska@op.pl>
2018-12-24 00:13:15 +0100

    Create a Modal With HTML, CSS & JavaScript
    Changes to be committed:
    	new file:   TraversyMedia/Modal_JS-CSS-HTML/index.html
    	new file:   TraversyMedia/Modal_JS-CSS-HTML/index2.html
    	new file:   TraversyMedia/Modal_JS-CSS-HTML/main.css
    	new file:   TraversyMedia/Modal_JS-CSS-HTML/main.js
    	new file:   TraversyMedia/Modal_JS-CSS-HTML/main2.css

---
+++ TraversyMedia/Modal_JS-CSS-HTML/main.js
@@ -0,0 +1,22 @@
+const modal = document.getElementById('simpleModal');
+const modalBtn = document.getElementById('modalBtn');
+const closeBtn = document.querySelector('.closeBtn');
+
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', clickOutside);
+
+function openModal(){
+  modal.style.display = 'block';
+}
+
+function closeModal(){
+  modal.style.display = 'none';
+}
+
+function clickOutside(e){
+  if(e.target === modal){
+    modal.style.display = 'none';
+  }
+}
+
