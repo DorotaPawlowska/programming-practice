be99a2fca75496c6f149b4dc11c4071014109d64
Dorota Pawłowska <dpawlowska@op.pl>
2019-01-13 00:17:46 +0100

    Sticky notes cz.2 - zapisywanie notatek do localStorage
    Changes to be committed:
    	modified:   JustForFun/Koduje/Koduje-StickyNotes/index.html
    	modified:   JustForFun/Koduje/Koduje-StickyNotes/main.css
    	modified:   JustForFun/Koduje/Koduje-StickyNotes/main.js

--- JustForFun/Koduje/Koduje-StickyNotes/main.js
+++ JustForFun/Koduje/Koduje-StickyNotes/main.js
@@ -8,7 +8,14 @@
       grabPointY,
       grabPointX,
       createNote,
-      addNoteBtnEl;
+      addNoteBtnEl,
+      init,
+      testLocalStorage,
+      saveNote,
+      deleteNote,
+      loadNotes,
+      getNoteObject,
+      onAddNoteBtnClick;
 
   onDragStart = function(ev){
 
@@ -43,17 +50,71 @@
     grabPointY = null;
   };
 
-  createNote = function(){
+  getNoteObject = function(el){
+    var textarea = el.querySelector('textarea');
+    return{
+      content: textarea.value,
+      id: el.id,
+      transformCSSValue: el.style.transform,
+      textarea: {
+        width: textarea.style.width,
+        height: textarea.style.height,
+      }
+    }
+  };
+
+  createNote = function(options){
     var stickerEl = document.createElement('div'),
         barEl = document.createElement('div'),
-        textareaEl = document.createElement('textarea');
+        textareaEl = document.createElement('textarea'),
+        saveBtnEl = document.createElement('button'),
+        deleteBtnEl = document.createElement('button'),
+        onSave,
+        onDelete,
+        noteConfig = options ||{
+          content: '',
+          id: "sticker_" + new Date().getTime(),
+          transformCSSValue: "translateX(" + Math.random()*400 + "px) translateY(" + Math.random()*400 + "px)"
+        };
 
-    var transformCSSValue = "translateX(" + Math.random()*400 + "px) translateY(" + Math.random()*400 + "px)";
+    onDelete = function(){
+      deleteNote(
+        getNoteObject(stickerEl)
+      );
+      document.body.removeChild(stickerEl);
+    };
 
-    stickerEl.style.transform = transformCSSValue;
+    onSave = function(){
+      // console.log(getNoteObject(stickerEl));
+      saveNote(
+        getNoteObject(stickerEl)
+      );
+    };
+
+    if(noteConfig.textarea){
+      textareaEl.style.width = noteConfig.textarea.width;
+      textareaEl.style.height = noteConfig.textarea.height;
+      textareaEl.style.resize = 'none';
+    }
+
+    stickerEl.id = noteConfig.id;
+    textareaEl.value = noteConfig.content;
+
+    saveBtnEl.addEventListener('click', onSave, false);
+    deleteBtnEl.addEventListener('click', onDelete, false);
+
+    saveBtnEl.classList.add('saveBtn');
+    deleteBtnEl.classList.add('deleteBtn');
+
+
+    console.log(noteConfig);
+    stickerEl.style.transform = noteConfig.transformCSSValue;
 
     barEl.classList.add('bar');
     stickerEl.classList.add('sticker');
+
+    barEl.appendChild(saveBtnEl);
+    barEl.appendChild(deleteBtnEl);
 
     stickerEl.appendChild(barEl);
     stickerEl.appendChild(textareaEl);
@@ -61,11 +122,60 @@
     document.body.appendChild(stickerEl);
   };
 
-  // createNote();
-  addNoteBtnEl = document.querySelector('.addNoteBtn');
-  addNoteBtnEl.addEventListener('click', createNote, false);
-  document.addEventListener('mousemove', onDrag, false);
-  document.addEventListener('mouseup', onDragEnd, false);
-  // document.querySelector('.sticker').addEventListener('mousedown', onDragStart, false);
+  testLocalStorage = function(){
+    var foo = 'foo';
+    try{
+      localStorage.setItem(foo, foo);
+      localStorage.removeItem(foo);
+      return true;
+    } catch(e){
+      return false;
+    }
+  };
 
+  onAddNoteBtnClick = function(){
+    createNote();
+  };
+
+  init = function(){
+
+    if(!testLocalStorage()){
+      var message = 'NO!';
+      saveNote = function(){
+        console.warn(message);
+      };
+      deleteNote = function(){
+        console.warn(message);
+      };
+    }else{
+      saveNote = function(note){
+        localStorage.setItem(note.id, JSON.stringify(note));
+        //notatka zapis
+      };
+      deleteNote = function(note){
+        localStorage.removeItem(note.id);
+        //notatka usuwanie
+      };
+
+      loadNotes = function(){
+        for(var i = 0; i < localStorage.length; i++){
+          var noteObj = JSON.parse(
+            localStorage.getItem(
+              localStorage.key(i)
+            )
+          );
+          createNote(noteObj);
+        }
+        //notatki wypisanie
+      };
+      loadNotes();
+    }
+
+    addNoteBtnEl = document.querySelector('.addNoteBtn');
+    addNoteBtnEl.addEventListener('click', onAddNoteBtnClick, false);
+    document.addEventListener('mousemove', onDrag, false);
+    document.addEventListener('mouseup', onDragEnd, false);
+  };
+
+  init();
 })();
