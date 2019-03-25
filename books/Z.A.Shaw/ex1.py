22a6d698c717ed5b2813d4d2d37b421f8e3bf9ff
dorota <dpawlowska@op.pl>
2019-03-25 10:32:49 +0100

    new book to work with - lerning Python (Zed A. Shaw)
    Changes to be committed:
    	renamed:    _inne/BuildAWeatherAppWithVanillaJS/index.html -> DevEd/BuildAWeatherAppWithVanillaJS/index.html
    	renamed:    _inne/BuildAWeatherAppWithVanillaJS/main.css -> DevEd/BuildAWeatherAppWithVanillaJS/main.css
    	renamed:    _inne/BuildAWeatherAppWithVanillaJS/main.js -> DevEd/BuildAWeatherAppWithVanillaJS/main.js
    	renamed:    _inne/BuildAWeatherAppWithVanillaJS/skycons.js -> DevEd/BuildAWeatherAppWithVanillaJS/skycons.js
    	new file:   books/Z.A.Shaw/ex1.py

---
+++ books/Z.A.Shaw/ex1.py
@@ -0,0 +1,8 @@
+print("Witaj, świecie!")
+# print("Witaj ponownie")
+# print("Podoba mi się takie wpisywanie.")
+# print("To jest fajne.")
+# print('Jejku! Drukuje.')
+# print("Wolałbym, żebyś tego 'nie robił'.")
+# print('"Powiedziałem", że masz tego nie dotykać.')
+# print('Kolejna linia tekstu.')
