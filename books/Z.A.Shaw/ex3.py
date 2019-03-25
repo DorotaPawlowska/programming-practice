cfd31432359e3002fa35cef194d621efbb0a64c0
dorota <dpawlowska@op.pl>
2019-03-25 11:46:06 +0100

    lerning Python (3) - algebra
    Changes to be committed:
    	modified:   books/Z.A.Shaw/ex3.py
    	new file:   books/Z.A.Shaw/ob1.py

---
+++ books/Z.A.Shaw/ex3.py
@@ -0,0 +1,31 @@
+print("Policzę teraz pogłowie mojego drobiu: ")
+
+# operacje matematyczne
+print("Kury", 25 + 30 / 6)
+print("Kury", 25.0 + 30.0 / 6.0)
+# operacje matematyczne
+print("Koguty", 100 - 25 * 3 % 4)
+print("Koguty", 100.0 - 25.0 * 3.0 % 4.0)
+
+print("Teraz policze jaja: ")
+# więcej operacji matematycznych
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+print("Czy to prawda, że 3 + 2 < 5 - 7?")
+# jeszcze więcej operacji matematycznych
+print(3 + 2 < 5 -7)
+# dodawanie
+print("Ile to jest 3 + 2?", 3 + 2)
+# odejmowanie
+print("Ile to jest 5 - 7?", 5 - 7)
+
+print("Oj, to dlatego to daje False.")
+
+print("Poćwiczmy jeszcze trochę.")
+# większe
+print("Czy to jest większe?", 5 > -2)
+# większelub równe
+print("Czy to jest większe lub równe?", 5>= -2)
+# mniejsze lub równe
+print("Czy to jest mniejsze lub równe?", 5 <= -2)
---
+++ books/Z.A.Shaw/ob1.py
@@ -0,0 +1,17 @@
+print(" 97 % 3 ")
+print("wynik tych ważnych obliczeń to: ", 97 % 3 )
+
+print(" 99 / 3 ")
+print("wynik tych ważnych obliczeń to: ", 99 / 3 )
+
+print(" 99 + 3 ")
+print("wynik tych ważnych obliczeń to: ", 99 + 3 )
+
+print(" 99 - 3 ")
+print("wynik tych ważnych obliczeń to: ", 99 - 3 )
+
+print(" 99 * 3 ")
+print("wynik tych ważnych obliczeń to: ", 99 * 3 )
+
+print(" 9 ** 3 ")
+print("wynik tych ważnych obliczeń to: ", 9 ** 3 )
