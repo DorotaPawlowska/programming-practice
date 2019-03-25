4e46038cd768f43f1bd462f4b6853d37e55f1302
dorota <dpawlowska@op.pl>
2019-03-25 11:14:45 +0100

    lerning Python (2)- comments
    Changes to be committed:
    	new file:   books/Z.A.Shaw/ex2.py
    	new file:   books/Z.A.Shaw/ex3.py

---
+++ books/Z.A.Shaw/ex2.py
@@ -0,0 +1,9 @@
+# Komentarz; ułatwia późniejsze czytanie programu
+# Wszystko, co znajduje się po znaku #, jest przez Pythona ignorowane.
+
+print("Mógłbym napisać taki kod.") # a komentarz po kodzie będzie ignorowany
+
+# Komentarza możesz również użyć do "wyłączenia", czyli wykomentowania kodu: 
+# print("To nie zostanie uruchomione.")
+
+print("To zostanie uruchomione")
