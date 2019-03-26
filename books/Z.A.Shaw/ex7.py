d4c289a911cad98b0938b27c28de5816faf233aa
dorota <dpawlowska@op.pl>
2019-03-26 19:46:12 +0100

    lerning Python (7) - more printing
    Changes to be committed:
    	new file:   books/Z.A.Shaw/ex7.py

---
+++ books/Z.A.Shaw/ex7.py
@@ -0,0 +1,24 @@
+print("Mary małą owcę ma.")
+# formatowanie łańcuch tekstu funkcją format()
+print("Która biała jest jak {}.".format('mgła'))
+print("Raz do szkoły przyszły dwie.")
+# zapis 10ciu kropek
+print(("." * 10 + ' ') * 2) # co to będzie robić?
+
+# przypisanie liter do zmiennych
+end1 ="C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# zwróć uwagę na przecinek na końcu; spróbuj go usunąć, żeby zobaczyć, co się stanie
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 , end8 , end9 , end10 , end11 , end12)
