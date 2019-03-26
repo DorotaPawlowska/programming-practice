db352d13f7406112d7796c14afa7a4ff45ed2dec
dorota <dpawlowska@op.pl>
2019-03-26 21:28:37 +0100

    lerning Python (9) - printing times 3
    Changes to be committed:
    	new file:   books/Z.A.Shaw/ex9.py

---
+++ books/Z.A.Shaw/ex9.py
@@ -0,0 +1,14 @@
+# Tu jest trochę nowych, dziwnych rzeczy; pamiętaj, żeby wpisać wszystko dokładnie
+
+days = "Pon Wt Śr Czw Pt Sob Niedz"
+months = "Sty\nLut\nMar\nKwi\nMaj\nCze\nLip\nSie"
+
+print("Oto dni tygodnia: ", days)
+print("Oto miesiące: ", months)
+
+print("""
+Tutaj coś się dzieje.
+Używamy trzech podwójnych cudzysłowów.
+Będziemy w stanie wpisac dowolnie dużo tekstu.
+Jeśli chcemy, nawet 4 linie albo 5 lub 6.
+""")
