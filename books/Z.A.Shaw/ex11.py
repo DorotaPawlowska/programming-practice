328a1b16a6bed76ab54a5b334e0f3fe1b0eb7944
dorota <dpawlowska@op.pl>
2019-03-27 21:04:17 +0100

    lerning Python (12) - input() function Changes to be committed: 	new file:   books/Z.A.Shaw/ex11.py

---
+++ books/Z.A.Shaw/ex11.py
@@ -0,0 +1,17 @@
+print("Ile masz lat?", end=' ')
+age = input()
+print("Ile masz wzrostu?", end=' ')
+height = input()
+print("Ile ważysz?", end=' ')
+weight = input()
+
+wiek = input("Ile masz lat? ")
+wzrost = input("Ile masz wzrostu? ")
+waga = input("Ile ważysz? ")
+
+x = int(input("podaj x: "))
+y = int(input("podaj y: "))
+
+print(f" x={x} razy y={y} wynosi: {x*y}")
+print(f"Więc masz {age} lat, {height} wzrostu i ważysz {weight}.")
+print(f"Więc masz {wiek} lat, {wzrost} wzrostu i ważysz {waga}.")
