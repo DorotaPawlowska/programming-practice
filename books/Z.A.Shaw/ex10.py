88f0dba25bb932df9d1547243a09bf9f3018aa4e
dorota <dpawlowska@op.pl>
2019-03-27 20:48:06 +0100

    lerning Python (10) - UNICODE and escape sequences
    Changes to be committed:
    	new file:   books/Z.A.Shaw/ex10.py

---
+++ books/Z.A.Shaw/ex10.py
@@ -0,0 +1,33 @@
+tabby_cat = "\tJestem tabulatorem."
+persian_cat = "Jestem podziałem\nlinii."
+backslash_cat = "Jestem sobie \\ jakimś \\ kotem."
+
+cat = "kotka"
+
+fat_cat = f'''
+Zrobię listę:
+\t* Jedzenie dla {cat}
+\t* Rybki
+\t* Kocimiętka\n\t* Trawa
+'''
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+
+# print("\\")
+# print("\'")
+# print("\"")
+# print("\a")
+# print("\b")
+# print("\f")
+# print("\n")
+# print("\N{GRINNING FACE}")
+# print("\r")
+# print("\t")
+print("\u2665")
+print("\U0001f638")
+# print("\v")
+print("\041")
+print("\x35")
