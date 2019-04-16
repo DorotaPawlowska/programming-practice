eee43309515e6905bc4590a91aceef31e8f655e3
dorota <dpawlowska@op.pl>
2019-04-16 20:26:20 +0200

    Memory Card Game - JavaScript Tutorial part II - more advanced JavaScript
    Changes to be committed:
    	modified:   FreeCodeCamp/JSMemoryCardGame/app.js

--- FreeCodeCamp/JSMemoryCardGame/app.js
+++ FreeCodeCamp/JSMemoryCardGame/app.js
@@ -1,26 +1,56 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
-
-
 function flipCard(){
+    if(lockBoard) return;
+    if(this === firstCard) return;
     this.classList.add('flip');
 
     if(!hasFlippedCard){
-        // fisrt click
+        // first card clicked
         hasFlippedCard = true;
         firstCard = this;
-        // console.log({hasFlippedCard, firstCard});
-    } else {
-        // second card clicked
-        hasFlippedCard = false;
-        secondCard = this;
-        // console.log({firstCard, secondCard});
-        // do cards macht?
-
+        return;
     }
+    // second card clicked
+    hasFlippedCard = false;
+    secondCard = this;
+    checkForMatch();
 }
+
+function checkForMatch(){
+    let isMatch = firstCard.dataset.frame === secondCard.dataset.frame;
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+}
+
+function unflipCards(){
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false]; // destrukturyzacja
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle(){
+    cards.forEach( card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
 
 cards.forEach( card => card.addEventListener('click', flipCard));
