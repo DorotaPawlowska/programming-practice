7a0c8674d997f9c15c38531ae8fd5d75100cad0b
dorota <dpawlowska@op.pl>
2019-04-30 23:46:11 +0200

    lerning Python (29) - what IF
    Changes to be committed:
    	new file:   books/Z.A.Shaw/ex21-30/ex29.py

---
+++ books/Z.A.Shaw/ex21-30/ex29.py
@@ -0,0 +1,27 @@
+people = 2
+cats = 13
+dogs = 5
+
+if (people < cats) and (cats == dogs):
+    print("Zbyt dużo kotów! Świat jest skazany na zagładę!")
+
+if people > cats:
+    print("Nie za dużo kotów! Świat jest ocalony!")
+
+if people < dogs:
+    print("Świat się ślini!")
+
+if people > dogs:
+    print("Świat jest suchy!")
+
+dogs += 5
+
+if people >= dogs:
+    print("Liczba ludzi jest większa lub równa liczbie psów.")
+
+if people <= dogs:
+    print("Liczba ludzi jest mniejsza lub równa liczbie psów.")
+
+if people == dogs:
+
+    print("Ludzie są psami.")
