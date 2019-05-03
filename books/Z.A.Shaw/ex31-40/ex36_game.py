2ec4d51f71e6bf610c66d7afabcd7a7abc122837
dorota <dpawlowska@op.pl>
2019-05-03 15:05:09 +0200

    lerning Python (36) - "SIMPLE EYE" GAME - completed
    Changes to be committed:
    	modified:   books/Z.A.Shaw/ex31-40/ex36_game.py

--- books/Z.A.Shaw/ex31-40/ex36_game.py
+++ books/Z.A.Shaw/ex31-40/ex36_game.py
@@ -4,53 +4,59 @@ import random
 
 def rulesOfTheGame():
     print("SIMPLE EYE")
-    print("gra polegająca na dobieraniu cyfr z zakresu od 2 do 10 tak by ich suma wyniosła 21")
-    print("czy chcesz zagrać Y/N")
-    answer = input("> ")
+    print("gra polegająca na dobieraniu losowych cyfr z zakresu od 2 do 10 tak by ich suma wyniosła 21")
+    print("czy chcesz zagrać TAK/NIE")
+    userAnswer = input("> ")
 
-    if answer == "Y":
-        return True
-    else:
+    if userAnswer == "N":
         return False
-
+    else:
+        return True
 
 def drawingNumbers():
-
     yes = True
-    tablica = []
+    answers = []
 
     while yes:
-        liczba = random.randint(2,10)
-        tablica.append(liczba)
-        print("wylosowana liczba to: ", liczba)
+        number = random.randint(2,10)
+        answers.append(number)
+        print("wylosowana liczba to: ", number)
         print("dodać kolejną liczbę do kolekcji TAK/NIE")
-        qContinue = input("> ")
-        if qContinue == "NIE" or qContinue == "N":
+        userAnswer = input("> ")
+        if userAnswer == "NIE" or userAnswer == "N":
             yes = False
 
-    print("twoje liczby: ", tablica)
-    return tablica
+    print("twoje wylosowane liczby: ", answers)
+    return answers
 
+def sumAllNumbers(collection):
+    added = 0
+    for i in collection:
+        added += i
+    return added 
 
-def sumAllNumbers(tab):
-    suma = 0
-    for i in tab:
-        suma += i
-    return suma 
+def winOrLoose(score):
+    win = 21
+    if score == win:
+        print("Wygrana! Gratulacje")
+    elif score > win:
+        print("przegrana - za dużo")
+    else:
+        print("przegrana - za mało")
 
+def startGame():
+    play = rulesOfTheGame()
+
+    while play:
+        collection = drawingNumbers()
+        score = sumAllNumbers(collection)
+        print("twój wynik: ", score)
+        winOrLoose(score)
+
+        print("czy chcesz zagrać jeszcze raz? (TAK/NIE)")
+        userAnswer = input("> ")
+        if userAnswer == "NIE" or userAnswer == "N":
+            play = False
 
 # GAME
-
-play = rulesOfTheGame()
-
-while play:
-
-    tab = drawingNumbers()
-    wynik = sumAllNumbers(tab)
-    print("twój wynik: ", wynik)
-
-
-    print("aby skończyć grę wpisz NIE albo N")
-    qContinue = input("> ")
-    if qContinue == "NIE" or qContinue == "N":
-        play = False
+startGame()
