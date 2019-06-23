9fa2a3e5962240831dc4b73301dc3a1f62b39bfa
dorota <dpawlowska@op.pl>
2019-06-23 21:17:46 +0200

    JavaScript Factory Functions
    Changes to be committed:
    	new file:   withMosh/JS-FactoryFunctions/index.html
    	new file:   withMosh/JS-FactoryFunctions/index.js

---
+++ withMosh/JS-FactoryFunctions/index.js
@@ -0,0 +1,17 @@
+// Factory Function
+
+function createCircle(radius){
+    return {
+        radius,
+        draw(){
+            console.log('draw');
+        }
+    };
+}
+
+const circle1 = createCircle(1);
+console.log(circle1);
+circle1.draw();
+
+const circle2 = createCircle(2);
+console.log(circle2);
