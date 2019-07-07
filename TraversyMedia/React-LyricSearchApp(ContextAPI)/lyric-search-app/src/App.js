737928ad28e021673c7b500e38ce54f3f4d8bc69
dorota <dpawlowska@op.pl>
2019-07-07 16:19:59 +0200

    Lyric Search App With React & Context API [2] - Lyrics Page
    Changes to be committed:
    	modified:   TraversyMedia/React-LyricSearchApp(ContextAPI)/lyric-search-app/package.json
    	modified:   TraversyMedia/React-LyricSearchApp(ContextAPI)/lyric-search-app/src/App.js
    	new file:   TraversyMedia/React-LyricSearchApp(ContextAPI)/lyric-search-app/src/components/tracks/Lyrics.js

--- TraversyMedia/React-LyricSearchApp(ContextAPI)/lyric-search-app/src/App.js
+++ TraversyMedia/React-LyricSearchApp(ContextAPI)/lyric-search-app/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index'; 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Provider from './context';
+import Lyrics from './components/tracks/Lyrics';
 
 import './App.css';
 
@@ -15,6 +16,7 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path="/" component={Index} />
+              <Route exact path="/lyrics/track/:id" component={Lyrics} />
             </Switch>
           </div>
         </React.Fragment>
