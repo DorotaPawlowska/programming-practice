06230229b95ef2de8156b4e774ffbabaa7962060
dorota <dpawlowska@op.pl>
2019-07-09 21:23:32 +0200

    Build a Custom URL Shortner Service
    Changes to be committed:
    	new file:   TraversyMedia/mongoDB_CustomURLShortnerService/.gitignore
    	new file:   TraversyMedia/mongoDB_CustomURLShortnerService/config/db.js
    	new file:   TraversyMedia/mongoDB_CustomURLShortnerService/config/default.json
    	new file:   TraversyMedia/mongoDB_CustomURLShortnerService/index.js
    	new file:   TraversyMedia/mongoDB_CustomURLShortnerService/models/Url.js
    	new file:   TraversyMedia/mongoDB_CustomURLShortnerService/package.json
    	new file:   TraversyMedia/mongoDB_CustomURLShortnerService/requests/api.http
    	new file:   TraversyMedia/mongoDB_CustomURLShortnerService/routes/index.js
    	new file:   TraversyMedia/mongoDB_CustomURLShortnerService/routes/url.js

---
+++ TraversyMedia/mongoDB_CustomURLShortnerService/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// connect to database
+connectDB();
+
+app.use(express.json({ extended: false }));
+
+// define routes
+app.use('/', require('./routes/index'))
+app.use('/api/url', require('./routes/url'))
+
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ TraversyMedia/mongoDB_CustomURLShortnerService/routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+const Url = require('../models/Url');
+
+//  @route GET /:code
+//  @desc  redirect to long/orginal URL
+
+router.get('/:code', async (req,res) => {
+  try {
+    const url = await Url.findOne({ urlCode: req.params.code });
+
+    if(url){
+      return res.redirect(url.longUrl);
+    } else {
+      return res.status(404).json('no url found');
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json('Server error');
+  }
+});
+
+module.exports = router;
