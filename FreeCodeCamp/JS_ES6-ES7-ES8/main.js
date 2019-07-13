cdf60a5ac5a24b0c548ea44ff125fcea1f29f93f
dorota <dpawlowska@op.pl>
2019-07-13 21:50:53 +0200

    JavaScript ES6, ES7, ES8: Learn to Code on the Bleeding Edge (Full Course) -  Template Literals
    Changes to be committed:
    	new file:   FreeCodeCamp/JS_ES6-ES7-ES8/index.html
    	new file:   FreeCodeCamp/JS_ES6-ES7-ES8/main.js

---
+++ FreeCodeCamp/JS_ES6-ES7-ES8/main.js
@@ -0,0 +1,66 @@
+// ⭐️ Course content ⭐️
+
+// ⌨️ Template Literals (1:06)
+let word1 = "Dylan";
+let word2 = "Israel";
+
+let num1 = 2;
+let num2 = 3;
+
+
+// old way
+const fullName = word1 + ' ' + word2;
+let example = 'hello \n' + 'world';
+
+// new way
+const fullName2 = `${word1} ${word2}`;
+const sum = `${num1 + num2}`;
+let example2 = `${word1}
+hello
+world
+${word2}
+`;
+
+console.log(fullName);
+console.log(fullName2);
+console.log(sum);
+console.log(example);
+console.log(example2);
+
+document.getElementById('example').innerText = example;
+document.getElementById('example2').innerText = example2;
+
+
+
+
+// ⌨️ Destructuring (5:32)
+
+// ⌨️ Object Literal (9:42)
+
+// ⌨️ For of loop (13:44)
+
+// ⌨️ Spread operator (17:51) 
+
+// ⌨️ Rest operator (20:57)
+
+// ⌨️ Arrow Functions (23:02)
+
+// ⌨️ Default Params (25:50)
+
+// ⌨️ Array.includes() (27:44)
+
+// ⌨️ Let & const (28:58)
+
+// ⌨️ Export & import (32:53)
+
+// ⌨️ String.padStrart(), String.padEnd() (35:29)
+
+// ⌨️ Classes (38:05)
+
+// ⌨️ Trailing commas (49:59)
+
+// ⌨️ Async/Await (51:27)
+
+// ⌨️ Sets (1:01:19)
+
+// ⌨️ What's Next (1:04:08)
