d3b1c5f917304b248ed3ee70085d518f442242e5
dorota <dpawlowska@op.pl>
2019-09-05 20:58:48 +0200

    VSC configuration for c/c++ programming
    Changes to be committed:
    	new file:   c-cpp/test/main.c
    	new file:   c-cpp/test/main.cpp
    	new file:   c-cpp/test/test.exe
    	new file:   c-cpp/test/test2.exe

---
+++ c-cpp/test/main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(void)
+{
+  printf("\n The Geek Stuff\n");
+  return 0;
+}
---
+++ c-cpp/test/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream> 
+int main() 
+{ 
+  std::cout << "Hello Geek\n"; 
+  return 0; 
+} 
