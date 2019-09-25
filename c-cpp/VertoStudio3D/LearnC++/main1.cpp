11b87879eeb7a65a2879c547596214412b52fa56
dorota <dpawlowska@op.pl>
2019-09-25 20:08:27 +0200

    Learn C++ :: Episode 0 :: Intro
    Changes to be committed:
    	new file:   c-cpp/VertoStudio3D/LearnC++/main1.cpp
    	new file:   c-cpp/VertoStudio3D/LearnC++/main1.exe

---
+++ c-cpp/VertoStudio3D/LearnC++/main1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// RidiciloulslyLongType::SubType function(){
+
+// }
+
+int main(){
+
+  bool b = true; // or false
+
+  int32_t i32 = 1;
+  uint64_t u64 = 0;
+  int8_t i8 = 0;
+  uint8_t u8;
+
+  const char *str = "hi there";
+  string cppstr = "hi there";
+
+  auto a = 1.0f;
+  auto a2 = 1;
+  auto a3 = (double)1.0f;
+
+  // RidiciloulslyLongType::SubType ret = function();
+  // auto ret = function();
+
+  return 0;
+}
