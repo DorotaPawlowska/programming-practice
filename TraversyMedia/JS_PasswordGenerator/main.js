b4f74b78ea01f8cbf4882ed2839725df2872be45
dorota <dpawlowska@op.pl>
2019-10-23 22:22:37 +0200

    JavaScript Password Generator - part A
    Changes to be committed:
    	new file:   FlorinPop/#100DaysOfCode/001/index.html
    	new file:   FlorinPop/#100DaysOfCode/001/main.js
    	new file:   FlorinPop/#100DaysOfCode/001/style.css
    	new file:   TraversyMedia/JS_PasswordGenerator/index.html
    	new file:   TraversyMedia/JS_PasswordGenerator/main.js
    	new file:   TraversyMedia/JS_PasswordGenerator/style.css

---
+++ TraversyMedia/JS_PasswordGenerator/main.js
@@ -0,0 +1,35 @@
+// DOM elements
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+};
+
+// generator function
+
+function getRandomLower(){
+  return String.fromCharCode(Math.floor(Math.random()*26)+97);
+}
+
+function getRandomUpper(){
+  return String.fromCharCode(Math.floor(Math.random()*26)+65);
+}
+
+function getRandomNumber(){
+  return String.fromCharCode(Math.floor(Math.random()*10)+48);
+}
+
+function getRandomSymbol(){
+  const symbols = "!@#$%^&*(){}[]=<>/,.|?";
+  return symbols[Math.floor(Math.random() * symbols.length)]
+}
