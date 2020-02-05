23fb5aad35127f3b2281eb99044ce1ee9cd6c44c
dorota <dpawlowska@op.pl>
2020-02-05 21:20:47 +0100

    HTML5 Canvas API Crash Course - part II - paths
    Changes to be committed:
    	modified:   TraversyMedia/HTML5CanvasAPICrashCourse/script.js

--- TraversyMedia/HTML5CanvasAPICrashCourse/script.js
+++ TraversyMedia/HTML5CanvasAPICrashCourse/script.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+
+//BASICS
+
 // if(canvas.getContext){
   // sprawdzanie wsparcia
 // }
@@ -9,25 +12,75 @@ const ctx = canvas.getContext('2d');
 // canvas.width = window.innerWidth;
 
 // fillRect()
-ctx.fillStyle = 'skyblue';
-ctx.fillRect(20, 20, 150, 100);
-ctx.fillStyle = 'coral';
-ctx.fillRect(200, 20, 150, 100);
+// ctx.fillStyle = 'skyblue';
+// ctx.fillRect(20, 20, 150, 100);
+// ctx.fillStyle = 'coral';
+// ctx.fillRect(200, 20, 150, 100);
 
 // strokeRect()
-ctx.lineWidth = 5;
-ctx.strokeStyle = 'green';
-ctx.strokeRect(100, 200, 150, 100);
+// ctx.lineWidth = 5;
+// ctx.strokeStyle = 'green';
+// ctx.strokeRect(100, 200, 150, 100);
 
 // clearRect()
-ctx.clearRect(25,25, 100, 50)
+// ctx.clearRect(25,25, 100, 50);
 
 // fillText()
-ctx.font = '30px Arial';
-ctx.fillStyle = 'purple';
-ctx.fillText('hello world', 400, 50)
+// ctx.font = '30px Arial';
+// ctx.fillStyle = 'purple';
+// ctx.fillText('hello world', 400, 50);
 
 // strokeText()
-ctx.lineWidth = 1;
-ctx.strokeStyle = 'orange';
-ctx.strokeText('hello world', 400, 100);
+// ctx.lineWidth = 1;
+// ctx.strokeStyle = 'orange';
+// ctx.strokeText('hello world', 400, 100);
+
+
+// PATHS
+
+// triangle
+ctx.beginPath();
+ctx.moveTo(50, 50);
+ctx.lineTo(150, 50);
+ctx.lineTo(100, 200);
+// ctx.lineTo(50, 50);
+ctx.closePath();
+// ctx.stroke();
+ctx.fillStyle = 'coral';
+ctx.fill();
+
+ctx.beginPath();
+ctx.moveTo(200, 50);
+ctx.lineTo(150, 200);
+ctx.lineTo(250, 200);
+ctx.closePath();
+ctx.stroke();
+
+// rectangle
+ctx.beginPath();
+ctx.rect(300, 50, 150, 100);
+ctx.fillStyle = 'teal';
+ctx.fill();
+
+// arcs (circles)
+ctx.beginPath();
+// ctx.arc(400, 400, 40, 0, Math.PI * 2, true);
+
+const centerX = canvas.width / 2;
+const centerY = canvas.height / 2;
+
+// draw head
+ctx.arc(centerX, centerY, 100, 0, Math.PI * 2);
+//move to mouth
+ctx.moveTo(centerX+50, centerY);
+//draw the mouth
+ctx.arc(centerX, centerY, 50, 0, Math.PI, false);
+//move to left eye
+ctx.moveTo(centerX-30, centerY-40);
+//draw left eye
+ctx.arc(centerX-40, centerY-40, 10, 0, Math.PI * 2);
+//move to right eye
+ctx.moveTo(centerX+50, centerY-40);
+//draw right eye
+ctx.arc(centerX+40, centerY-40, 10, 0, Math.PI * 2);
+ctx.stroke();
