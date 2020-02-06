a372f46a1cd012887c37bcc77d4e7fbd35b12da4
dorota <dpawlowska@op.pl>
2020-02-06 21:24:41 +0100

    HTML5 Canvas API Crash Course - part II - paths B
    Changes to be committed:
      modified:   TraversyMedia/HTML5CanvasAPICrashCourse/script.js

--- TraversyMedia/HTML5CanvasAPICrashCourse/script.js
+++ TraversyMedia/HTML5CanvasAPICrashCourse/script.js
@@ -83,4 +83,15 @@ ctx.arc(centerX-40, centerY-40, 10, 0, Math.PI * 2);
 ctx.moveTo(centerX+50, centerY-40);
 //draw right eye
 ctx.arc(centerX+40, centerY-40, 10, 0, Math.PI * 2);
+
+// quadratic curve
+ctx.moveTo(75, 25);
+ctx.quadraticCurveTo(25, 25, 25, 62.5);
+ctx.quadraticCurveTo(25, 100, 50, 100);
+ctx.quadraticCurveTo(50, 120, 30, 125);
+ctx.quadraticCurveTo(60, 120, 65, 100);
+ctx.quadraticCurveTo(125, 100, 125, 62.5);
+ctx.quadraticCurveTo(125, 25, 75, 25);
+
 ctx.stroke();
+
