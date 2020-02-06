6801b3edbca8d997b6ea92834084c1319614bcf7
dorota <dpawlowska@op.pl>
2020-02-06 22:12:40 +0100

    HTML5 Canvas API Crash Course - part IV - animation 2 - end
    Changes to be committed:
    	modified:   TraversyMedia/HTML5CanvasAPICrashCourse/index.html
    	modified:   TraversyMedia/HTML5CanvasAPICrashCourse/script.js
    	modified:   TraversyMedia/HTML5CanvasAPICrashCourse/style.css

--- TraversyMedia/HTML5CanvasAPICrashCourse/script.js
+++ TraversyMedia/HTML5CanvasAPICrashCourse/script.js
@@ -98,41 +98,149 @@ const ctx = canvas.getContext('2d');
 
 // ANIMATIONs
 // 1
-const circle = {
-  x: 200,
+
+// const circle = {
+//   x: 200,
+//   y: 200,
+//   size: 30,
+//   dx: 5,
+//   dy: 4,
+// };
+
+// function drawCircle(){
+//   ctx.beginPath();
+//   ctx.arc(circle.x, circle.y, circle.size, 0, Math.PI * 2);
+//   ctx.fillStyle = 'purple';
+//   ctx.fill();
+// }
+
+// function update(){
+//   ctx.clearRect(0,0, canvas.width, canvas.height);
+//   drawCircle();
+
+//   // change position
+//   circle.x += circle.dx;
+//   circle.y += circle.dy;
+
+//   //detect side walls
+//   if(circle.x + circle.size > canvas.width || circle.x - circle.size < 0 ){
+//     circle.dx *= -1;
+//     // circle.dy *= -1;
+//   }
+//   // detect top and bottom walls
+//   if(circle.y + circle.size > canvas.height || circle.y - circle.size < 0 ){
+//     circle.dy *= -1;
+//     // circle.dy *= -1;
+//   }
+
+//   requestAnimationFrame(update);
+// }
+
+// update();
+
+
+// 2 - character
+
+const image = document.getElementById('source');
+
+const player = {
+  w: 50,
+  h: 70,
+  x: 20,
   y: 200,
-  size: 30,
-  dx: 5,
-  dy: 4,
+  speed: 7,
+  dx: 0,
+  dy: 0,
 };
 
-function drawCircle(){
-  ctx.beginPath();
-  ctx.arc(circle.x, circle.y, circle.size, 0, Math.PI * 2);
-  ctx.fillStyle = 'purple';
-  ctx.fill();
+function drawPlayer(){
+  ctx.drawImage(image, player.x, player.y, player.w, player.h);
+}
+
+function clear(){
+  ctx.clearRect(0,0,canvas.width, canvas.height);
+}
+
+function newPosition(){
+  player.x += player.dx;
+  player.y += player.dy;
+
+  detectWalls();
+}
+
+function detectWalls(){
+  // left wall
+  if(player.x < 0){
+    player.x = 0;
+  }
+
+  // right
+  if(player.x + player.w > canvas.width){
+    player.x = canvas.width - player.w;
+  }
+
+  // top
+  if(player.y < 0){
+    player.y = 0;
+  }
+
+  // bottom
+  if(player.y + player.h > canvas.height){
+    player.y = canvas.height - player.h;
+  }
 }
 
 function update(){
-  ctx.clearRect(0,0, canvas.width, canvas.height);
-  drawCircle();
 
-  // change position
-  circle.x += circle.dx;
-  circle.y += circle.dy;
-
-  //detect side walls
-  if(circle.x + circle.size > canvas.width || circle.x - circle.size < 0 ){
-    circle.dx *= -1;
-    // circle.dy *= -1;
-  }
-  // detect top and bottom walls
-  if(circle.y + circle.size > canvas.height || circle.y - circle.size < 0 ){
-    circle.dy *= -1;
-    // circle.dy *= -1;
-  }
+  clear();
+  drawPlayer();
+  newPosition();
 
   requestAnimationFrame(update);
 }
 
+function moveUp(){
+  player.dy = -player.speed;
+}
+function moveDown(){
+  player.dy = player.speed;
+}
+function moveRight(){
+  player.dx = player.speed;
+}
+function moveLeft(){
+  player.dx = -player.speed;
+}
+
+function keyDown(e){
+  // console.log(e.key);
+  if(e.key === 'ArrowRight' || e.key ==='Right'){
+    moveRight();
+  } else if(e.key === 'ArrowLeft' || e.key ==='Left'){
+    moveLeft();
+  } else if(e.key === 'ArrowUp' || e.key ==='Up'){
+    moveUp();
+  } else if(e.key === 'ArrowDown' || e.key ==='Down'){
+    moveDown();
+  }
+}
+
+function keyUp(e){
+  if(
+    e.key === 'ArrowRight' || 
+    e.key ==='Right' || 
+    e.key === 'ArrowLeft' || 
+    e.key ==='Left' || 
+    e.key === 'ArrowUp' || 
+    e.key ==='Up' || 
+    e.key === 'ArrowDown' || 
+    e.key ==='Down'){
+    player.dx = 0;
+    player.dy = 0;
+  }
+}
+
 update();
+
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
