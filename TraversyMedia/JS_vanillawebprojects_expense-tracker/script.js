d26d135a9ac4d511f3f6542e9e18aec80d5f1487
dorota <dpawlowska@op.pl>
2020-03-15 20:36:43 +0100

    vanillawebprojects/expense-tracker/
    Changes to be committed:
    	new file:   TraversyMedia/JS_vanillawebprojects_expense-tracker/index.html
    	new file:   TraversyMedia/JS_vanillawebprojects_expense-tracker/script.js
    	new file:   TraversyMedia/JS_vanillawebprojects_expense-tracker/style.css

---
+++ TraversyMedia/JS_vanillawebprojects_expense-tracker/script.js
@@ -0,0 +1,100 @@
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+// const cummyTransactions = [
+//   { id: 1, text: 'flower', amount: -20},
+//   { id: 2, text: 'salary', amount: 300},
+//   { id: 3, text: 'book', amount: -10},
+//   { id: 4, text: 'camera', amount: 150},
+// ];
+
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem('transactions')
+);
+
+let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+
+// add transaction
+function addTransaction(e){
+  e.preventDefault();
+
+  if(text.value.trim() === '' || amount.value.trim() === ''){
+    alert('pleas add a text and amount');
+  } else {
+    const transaction = {
+      id: generateID(),
+      text: text.value,
+      amount: +amount.value
+    };
+    transactions.push(transaction);
+    addTransactionDOM(transaction);
+    updateValues();
+    updateLocalStorage();
+    text.value = '';
+    amount.value = '';
+  }
+}
+
+// random id
+function generateID(){
+  return Math.floor(Math.random() * 100000000);
+}
+
+// add transaction to DOM list
+function addTransactionDOM(transaction){
+  // get sign
+  const sign = transaction.amount < 0 ? '-' : '+';
+  const item = document.createElement('li');
+  // add class based on value
+  item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+  item.innerHTML = `
+    ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
+    <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
+  `;
+  list.appendChild(item);
+}
+
+// update balance, income and expense
+function updateValues(){
+  const amounts = transactions.map(transaction => transaction.amount);
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  const income = amounts
+    .filter(item => item> 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  const expense = (amounts
+    .filter(item => item < 0)
+    .reduce((acc, item) => (acc += item), 0) * -1).toFixed(2);
+
+  balance.innerText = `$${total}`;
+  money_plus.innerText = `$${income}`;
+  money_minus.innerText = `$${expense}`;
+}
+
+//remove transaction by id
+function removeTransaction(id){
+  transactions = transactions.filter(transaction => transaction.id !== id);
+  updateLocalStorage();
+  init();
+}
+
+// update local storage transactions
+function updateLocalStorage(){
+  localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
+// init app
+function init(){
+  list.innerHTML = '';
+  transactions.forEach(addTransactionDOM);
+  updateValues();
+}
+
+init();
+
+form.addEventListener('submit', addTransaction);
