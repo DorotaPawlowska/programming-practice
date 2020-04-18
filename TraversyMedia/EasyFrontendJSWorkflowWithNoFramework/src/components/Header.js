ecef95a4c214987a468533304079763e8bb6b56c
dorota <dpawlowska@op.pl>
2020-04-18 19:09:26 +0200

    Easy Frontend JS Workflow With No Framework
    Changes to be committed:
    	new file:   TraversyMedia/EasyFrontendJSWorkflowWithNoFramework/.babelrc
    	new file:   TraversyMedia/EasyFrontendJSWorkflowWithNoFramework/.gitignore
    	new file:   TraversyMedia/EasyFrontendJSWorkflowWithNoFramework/package.json
    	new file:   TraversyMedia/EasyFrontendJSWorkflowWithNoFramework/public/index.html
    	new file:   TraversyMedia/EasyFrontendJSWorkflowWithNoFramework/src/app.js
    	new file:   TraversyMedia/EasyFrontendJSWorkflowWithNoFramework/src/components/Header.js
    	new file:   TraversyMedia/EasyFrontendJSWorkflowWithNoFramework/src/components/User.js
    	new file:   TraversyMedia/EasyFrontendJSWorkflowWithNoFramework/src/scss/_card.scss
    	new file:   TraversyMedia/EasyFrontendJSWorkflowWithNoFramework/src/scss/app.scss

---
+++ TraversyMedia/EasyFrontendJSWorkflowWithNoFramework/src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  const template = `
+    <header>
+      <h1>My Parcel App</h1>
+      <p>lorem ipsum</p>
+    </header>
+  `;
+
+  return template;
+}
+
+export default Header;
