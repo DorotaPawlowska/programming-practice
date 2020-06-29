298a4f4eb86e821dabc8fa8a5feca45d63819999
dorota <dpawlowska@op.pl>
2020-06-29 17:00:16 +0200

    15#Building an Awesome Todo List App in React
    Changes to be committed:
    	new file:   books/K.Chinnathambi/LerningReact/todolist/.gitignore
    	new file:   books/K.Chinnathambi/LerningReact/todolist/README.md
    	new file:   books/K.Chinnathambi/LerningReact/todolist/package.json
    	new file:   books/K.Chinnathambi/LerningReact/todolist/public/index.html
    	new file:   books/K.Chinnathambi/LerningReact/todolist/src/TodoItems.js
    	new file:   books/K.Chinnathambi/LerningReact/todolist/src/TodoList.css
      new file:   books/K.Chinnathambi/LerningReact/todolist/src/TodoList.js
    	new file:   books/K.Chinnathambi/LerningReact/todolist/src/index.css
    	new file:   books/K.Chinnathambi/LerningReact/todolist/src/index.js

---
+++ books/K.Chinnathambi/LerningReact/todolist/src/TodoList.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import TodoItems from "./TodoItems.js";
+import "./TodoList.css";
+
+class TodoList extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      items: []
+    };
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+
+  addItem(e){
+    const itemArray = this.state.items;
+    if(this._inputElement.value !== ""){
+      itemArray.unshift({
+        text: this._inputElement.value,
+        key: Date.now()
+      });
+
+      this.setState({
+        items: itemArray
+      });
+
+      this._inputElement.value = "";
+    }
+    // console.log(itemArray);
+    e.preventDefault();
+  }
+
+  deleteItem(key){
+    const filteredItems = this.state.items.filter(function(item){
+      return (item.key !== key);
+    });
+
+    this.setState({
+      items: filteredItems
+    });
+  }
+
+  render(){
+    return(
+      <div className="todoListMain">
+        <div className="header">
+          <form onSubmit={this.addItem}>
+            <input  ref={(a) => this._inputElement = a} 
+                    placeholder="wpisz zadanie"/>
+            <button type="submit">dodaj</button>
+          </form>
+        </div>
+        <TodoItems entries={this.state.items}
+                    delete={this.deleteItem}/>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
