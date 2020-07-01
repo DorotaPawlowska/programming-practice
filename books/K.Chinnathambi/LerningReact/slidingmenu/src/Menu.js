0f40524d65cf98c47295e5c1d057f7cb21b8ef3f
dorota <dpawlowska@op.pl>
2020-07-01 21:03:17 +0200

    16#Creating a Sliding Menu in React
    Changes to be committed:
    	new file:   books/K.Chinnathambi/LerningReact/slidingmenu/.gitignore
    	new file:   books/K.Chinnathambi/LerningReact/slidingmenu/README.md
    	new file:   books/K.Chinnathambi/LerningReact/slidingmenu/package.json
    	new file:   books/K.Chinnathambi/LerningReact/slidingmenu/public/index.html
    	new file:   books/K.Chinnathambi/LerningReact/slidingmenu/src/ManuButton.js
    	new file:   books/K.Chinnathambi/LerningReact/slidingmenu/src/Menu.css
    	new file:   books/K.Chinnathambi/LerningReact/slidingmenu/src/Menu.js
    	new file:   books/K.Chinnathambi/LerningReact/slidingmenu/src/MenuButton.css
    	new file:   books/K.Chinnathambi/LerningReact/slidingmenu/src/MenuContainer.js
    	new file:   books/K.Chinnathambi/LerningReact/slidingmenu/src/index.css
    	new file:   books/K.Chinnathambi/LerningReact/slidingmenu/src/index.js

---
+++ books/K.Chinnathambi/LerningReact/slidingmenu/src/Menu.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./Menu.css";
+
+class Menu extends Component{
+  render(){
+    let visibility = "hide";
+    if(this.props.menuVisibility){
+      visibility = "show";
+    }
+
+    return(
+      <div id="flyoutMenu"
+            onMouseDown={this.props.handleMouseDown}
+            className={visibility}
+            >
+      <h2><a href="#">Strona główna</a></h2>
+      <h2><a href="#">O nas</a></h2>
+      <h2><a href="#">Kontakt</a></h2>
+      <h2><a href="#">Szukaj</a></h2>
+      </div>
+    );
+  }
+}
+
+export default Menu;
---
+++ books/K.Chinnathambi/LerningReact/slidingmenu/src/MenuContainer.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import MenuButton from "./ManuButton.js";
+import Menu from "./Menu.js";
+
+class MenuContainer extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      visible: false
+    };
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  handleMouseDown(e){
+    this.toggleMenu();
+    console.log("clicked");
+    e.stopPropagation();
+  }
+
+  toggleMenu(){
+    this.setState({
+      visible: !this.state.visible
+    });
+  }
+
+  render(){
+    return(
+      <div>
+        <MenuButton handleMouseDown={this.handleMouseDown} />
+        <Menu handleMouseDown={this.handleMouseDown}
+              menuVisibility={this.state.visible}/>
+        <div>
+          <p>Który z poniższych elementów nie pasuje do pozostałych?</p>
+          <ul>
+            <li>Drzewo</li>
+            <li>Trzcina</li>
+            <li>Ryba</li>
+            <li>Las</li>
+            <li>Trawa</li>
+            <li>Róża</li>
+            <li>Pomidor</li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default MenuContainer;
